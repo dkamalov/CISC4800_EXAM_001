d9830074976a9d96ed2c5b38c0090b1eb4bb5051
dkamalov <dkamalov8@gmail.com>
2020-11-22 14:20:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+// JavaScript source code
 function fetchData() {
 
-    return fetch('https://data.cityofnewyork.us/resource/rc75-m7u3.json').then(response -> response.json())
-                 
+    return fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json') // call on the api or place to pull data
+        .then(response => response.json()) // turn data retrieved into json once u get something back
+        .then(json => renderDatas(json)) // do something with the json
+
 }
 
 function renderDatas(json) {
-    
-    const h2 = document.querySelector('h2');
-    json.forEach(data => { 
-        const p = document.createElement('p')
-        p.innerHTML = Hospitilizations: ${data.hospitalized_count}
+    const h2 = document.querySelector('h2')
+    json.forEach(data => {
+        const p = document.createElement('p') // not inserting the element to browser yet 
+        // bringing an element to existence
+        p.innerHTML = `<strong>Year:</strong> ${data.year}`
         h2.appendChild(p)
     })
 }
 
-document.addEventListener('DOMContentLoaded', function () { fetchData() })
+document.addEventListener('DOMContentLoaded', function () {
+    fetchData()
+})
+
