a86281c56e93b96ee6b203c0c9d2c15bbf0e9aef
dkamalov <dkamalov8@gmail.com>
2020-11-22 13:37:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function fetchData() {
+    return fetch("https://data.cityofnewyork.us/resource/jb7j-dtam.json")
+    .then(response => response.json())
+    .then(json => renderData(json)); 
+}
 
+function renderData(json) {
+    const main = document.querySelector('main'); 
+    json.filter(data => data.sex == 'F' && data.year == 2014).forEach(element => {
+        const p = document.createElement('p');
+        p.innerHTML = Cause: ${element.leading_cause} Number: ${element.deaths}; 
+        main.appendChild(p);
+    });
+}
